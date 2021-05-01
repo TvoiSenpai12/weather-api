dfbd8897a30a935044af579801fbf16306ed9258
TvoiSenpai12 <74881055+TvoiSenpai12@users.noreply.github.com>
2021-05-01 03:49:02 +0300

    Delete assets directory

--- assets/script/main.js
+++
@@ -1,8 +0,0 @@
-// fetch('')
-//     .then(function (resp) { return resp.json() })
-//     .then(function (data) {
-//         console.log(data);
-//     })
-//     .catch(function () {
-
-//     });
